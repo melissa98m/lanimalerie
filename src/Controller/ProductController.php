a6cc3f44d1623f6e9f0b6c9bcc3a480de21f0d16
melissa98m <melissa.mangione@gmail.com>
2021-08-31 20:12:35 +0200

    filtre marche

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,9 +9,14 @@ use App\Form\SearchType;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 /**
  * @Route("/product")
@@ -35,34 +40,57 @@ class ProductController extends AbstractController
             
             $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
             
+        }else{
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
         }
         return $this->render('product/index.html.twig', [
-            'products' => $productRepository->findAll(),
+            'products' => $products,
             'form' => $form->createView(),
+            
         ]);
     }
 
     /**
      * @Route("/admin/new", name="product_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
-        $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
+        $product = new Product(); // crrer un nouveau product
+        $form = $this->createForm(ProductType::class , $product); // creer le formulaire
         $form->handleRequest($request);
+        //$entityManager->persist($formadd);
+        if ($form->isSubmitted() && $form->isValid()) {// condition si le formulaire est valide
+            $imageFile = $form->get('image')->getData(); //recuperer le contenue champ upload envoyé
 
-        if ($form->isSubmitted() && $form->isValid()) {
+            if($imageFile) {// condition si il y a un logo , traitement du fichier
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);// recupere le nom de ficher original uploader (juste le nom)
+                $safeFilename = $slugger->slug($originalFilename);// conversion le nom du fichier en un mon slug(nom de fichier sans escapace accent utilisable en url)
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();// crer un nouveau nom de fichier a partir du slug + id unique(pas de probleme upload fichier nom identique + extension (jpeg ...))
+                try{ 
+                    //copie le fichier upload sur le serveur avec le nouveau nom
+                    $imageFile->move($this->getParameter('upload_directory'), $newFilename);
+                }catch(FileException $e){
+                    var_dump($e);
+                    die('Erreur');
+                }
+                $product->setImage($newFilename);
+            }
+            
+            
+            
+            
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($product);
             $entityManager->flush();
-
+            //si tout est bon retourne sur la page listing produit
             return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
         }
-
+         //Page ajout produits avec formulaire
         return $this->renderForm('product/new.html.twig', [
-            'product' => $product,
+            'products' => $product,
             'form' => $form,
         ]);
+      
     }
 
     /**
@@ -78,23 +106,40 @@ class ProductController extends AbstractController
     /**
      * @Route("/admin/{id}/edit", name="product_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, Product $product): Response
+    public function edit(Request $request, Product $product , SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imageProduct')->getData(); //recuperer le contenue champ upload envoyé
+    
+            if($imageFile) {// condition si il y a un logo , traitement du fichier
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);// recupere le nom de ficher original uploader (juste le nom)
+                $safeFilename = $slugger->slug($originalFilename);// conversion le nom du fichier en un mon slug(nom de fichier sans escapace accent utilisable en url)
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();// crer un nouveau nom de fichier a partir du slug + id unique(pas de probleme upload fichier nom identique + extension (jpeg ...))
+                try{ 
+                    //copie le fichier upload sur le serveur avec le nouveau nom
+                    $imageFile->move($this->getParameter('upload_directory'), $newFilename);
+                }catch(FileException $e){
+                    var_dump($e);
+                    die('Erreur');
+                }
+                $product->setImage($newFilename);
+            }
+    
             $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
+    
+            return $this->redirectToRoute('list_Product', [], Response::HTTP_SEE_OTHER);
         }
-
+    
         return $this->renderForm('product/edit.html.twig', [
             'product' => $product,
             'form' => $form,
+        
         ]);
-    }
-
+    
+    }    
     /**
      * @Route("/admin/{id}", name="product_delete", methods={"POST"})
      */
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -3,28 +3,58 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Entity\Brand;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProductType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder , array $options)
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('image')
-            ->add('enstock')
-            ->add('reference')
-            ->add('description')
-        ;
+            ->add('name' , TextType::class)
+            ->add('price' , TextType::class)
+            ->add('EnStock' , ChoiceType::class , ['choices' =>['True' => 0,'false' => 1,]])
+            ->add('reference' , IntegerType::class)
+            ->add('image' , FileType::class , [
+                'label' => 'imageProduct' , 
+                'mapped' => false , // permet quil ne soit pas traiter directement
+                'required' => false , 
+                'constraints' => [  // ajoute de contrainte
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [ // types de fichiers que l'on veux recuperer
+                            'image/png',
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Merci de charger un format valide' ,
+                    ])
+                ]
+                ])
+            ->add('description' , TextType::class)
+            ->add('brand' , EntityType::class , array('class' => Brand::class, 
+                                                    'choice_label' => 'name'))
+            ->add('category' , EntityType::class , array('class' => Category::class, 
+                                                'choice_label' => 'name' , 
+                                                'multiple' => true , 
+                                                'expanded' => true))
+         ;  
+                                              
     }
-
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Product::class,
+
         ]);
     }
 }
